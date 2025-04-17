41aaf6838bfdc469aa4571a4d82e9ef39e60c2c5
keu210 <16938706+keu210@users.noreply.github.com>
2025-04-17 16:26:28 +0200

    feat: add keu210 guestbook entry with FriendlyCloudIcon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,11 +4,13 @@ import SmileyIcon from './SmileyIcon';
 import CompassIcon from './CompassIcon';
 import RobotIcon from './RobotIcon';
 
+import FriendlyCloudIcon from "./FriendlyCloudIcon";
 const iconComponents: Record<string, React.FC<any>> = {
   StarIcon,
   SmileyIcon,
   CompassIcon,
-  RobotIcon
+  RobotIcon,
+  FriendlyCloudIcon,
 };
 
 export default iconComponents;
