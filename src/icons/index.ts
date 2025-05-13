dee5e0249a4f405e511c25d46d3f5439699426ed
quin-srp <36859739+quin-srp@users.noreply.github.com>
2025-05-12 21:17:41 -0400

    Add CoffeeIcon guestbook entry for quin-srp

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,13 +4,15 @@ import SmileyIcon from './SmileyIcon';
 import CompassIcon from './CompassIcon';
 import RobotIcon from './RobotIcon';
 import MoonIcon from './MoonIcon';
+import CoffeeIcon from './CoffeeIcon';
 
 const iconComponents: Record<string, React.FC<any>> = {
   StarIcon,
   SmileyIcon,
   CompassIcon,
   RobotIcon,
-  MoonIcon
+  MoonIcon,
+  CoffeeIcon
 };
 
 export default iconComponents;
