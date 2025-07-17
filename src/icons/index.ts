fa1d7ab1f3e94264b7f52712425f2dfee1a14422
Isaac Gemal <isaacgemal@gmail.com>
2025-07-17 16:29:26 -0400

    feat(guestbook): add LightningIcon and entry for IsaacGemal

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,13 +4,15 @@ import SmileyIcon from './SmileyIcon';
 import CompassIcon from './CompassIcon';
 import RobotIcon from './RobotIcon';
 import MoonIcon from './MoonIcon';
+import LightningIcon from './LightningIcon';
 
 const iconComponents: Record<string, React.FC<any>> = {
   StarIcon,
   SmileyIcon,
   CompassIcon,
   RobotIcon,
-  MoonIcon
+  MoonIcon,
+  LightningIcon
 };
 
 export default iconComponents;
