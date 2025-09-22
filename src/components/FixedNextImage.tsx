b6564d1026f05ed55e630cc3b6a8becc2cc074f8
Isaac <147355120+IsaacGemal@users.noreply.github.com>
2025-09-22 16:42:31 -0400

    [HEA-1265] Upgrade Next.js from 13.5.11 to 15.4.1 with compatibility fixes (#53)
    * Upgrade Next.js from 13.5.11 to 15.4.1 with compatibility fixes
    
    * add next-env.d.ts to gitignore
    
    * Remove next-env.d.ts file

--- src/components/FixedNextImage.tsx
+++ src/components/FixedNextImage.tsx
@@ -4,14 +4,7 @@ import Image, { ImageProps } from "next/image";
 interface FixedNextImageProps extends ImageProps {}
 
 export function FixedNextImage(props: FixedNextImageProps) {
-  const { onLoadingComplete, ...rest } = props;
+  const { onLoad, ...rest } = props;
 
-  const handleLoadingComplete = (img: HTMLImageElement) => {
-    img.removeAttribute("fetchPriority");
-    if (onLoadingComplete) {
-      onLoadingComplete(img);
-    }
-  };
-
-  return <Image {...rest} onLoadingComplete={handleLoadingComplete} />;
+  return <Image {...rest} onLoad={onLoad} />;
 }
