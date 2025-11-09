45475a9b586deb720fcf87f4270f027023d455e5
Josh Robinson <hexorx@users.noreply.github.com>
2025-11-08 15:22:02 -1000

    Add hexorx guestbook entry with skull icon

---
+++ src/icons/SkullIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IconProps } from "../types";
+
+const SkullIcon: React.FC<IconProps> = ({ color = "currentColor", ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill={color}
+      {...props}
+    >
+      <path d="M12 2C8.13 2 5 5.13 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h2v3c0 .55.45 1 1 1s1-.45 1-1v-3h2c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.87-3.13-7-7-7zM9 11c-.83 0-1.5-.67-1.5-1.5S8.17 8 9 8s1.5.67 1.5 1.5S9.83 11 9 11zm6 0c-.83 0-1.5-.67-1.5-1.5S14.17 8 15 8s1.5.67 1.5 1.5S15.83 11 15 11zm-3 2c-1.03 0-1.9.62-2.28 1.5h4.56c-.38-.88-1.25-1.5-2.28-1.5z"/>
+    </svg>
+  );
+};
+
+export default SkullIcon;
