7d225de575e2fd308a0143fdba18e7ababd5f6c3
Alex Kayaian <akayaian@gmail.com>
2026-01-14 15:13:15 -0500

    Add guestbook entry for alex-lightshifthealth with butterfly icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -15,6 +15,7 @@ import SpiralIcon from './SpiralIcon';
 import FireworksIcon from './FireworksIcon';
 import PaperAirplaneIcon from './PaperAirplaneIcon';
 import RocketIcon from './RocketIcon';
+import ButterflyIcon from './ButterflyIcon';
 
 const iconComponents: Record<string, React.FC<any>> = {
   StarIcon,
@@ -32,7 +33,8 @@ const iconComponents: Record<string, React.FC<any>> = {
   SpiralIcon,
   FireworksIcon,
   PaperAirplaneIcon,
-  RocketIcon
+  RocketIcon,
+  ButterflyIcon
 };
 
 export default iconComponents;
